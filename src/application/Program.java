2f7de5ab075965816f4b50095352ba1a26df24c5
Bento Justino <bentojs@outlook.com.br>
2022-03-16 13:04:49 -0300

    Final

--- src/application/Program.java
+++ src/application/Program.java
@@ -46,7 +46,11 @@ public class Program {
 				
 				if (chessMatch.getPromoted() !=null) {
 					System.out.println("Enter piece for promotion (B/H/Q/R):");
-					String type = sc.nextLine();
+					String type = sc.nextLine().toUpperCase();
+					while (!type.equals("B") && !type.equals("H") && !type.equals("R") && !type.equals("Q")) {
+						System.out.println("Invalid value! Enter piece for promotion (B/H/Q/R):");
+						type = sc.nextLine().toUpperCase();
+					}
 					chessMatch.replacePromotedPiece(type);
 				}
 			}
